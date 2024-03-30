3fd504bcfb53f41a5f2ebfc1674fb07a59bb9155
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-03-30 18:27:57 +0530

    added Activity Page and resolved objectId error in converting

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,5 +1,7 @@
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 async function Page() {
@@ -10,10 +12,37 @@ async function Page() {
   const userInfo = await fetchUser(user.id);
   // ...
   if (!userInfo?.onboarded) redirect('/onboarding'); // Use the 'redirect' function
+        //get activity
+  const activity = await getActivity(userInfo._id);
     return (
-      <div className="head-text">Activity Page</div>
-      //get activity
-      
+      <section>
+      <h1 className="head-text">Activity Page</h1>
+        <section className='mt-10 flex flex-col gap-5'>
+          {activity.length > 0 ? (
+            <>
+            {activity.map((activity)  => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article>
+                  <Image 
+                  src={activity.author.image}
+                  alt="Profile picture"
+                  width={20}
+                  height={20}
+                  className="rounded-full"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your channel
+                  </p>
+                </article>
+              </Link>
+            ))}
+            </>
+          ): (<p className="!text-base-regular text-light-3">No activity found</p>)}
+        </section>
+      </section>
     )
   }
   
